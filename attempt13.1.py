c6d9666d30a017778f9230b0c20a021e13cb46eb
deadhead65 <37427871+deadhead65@users.noreply.github.com>
2018-03-21 11:54:40 -0400

    Add files via upload
    Right now i am trying to import argv from system,
    i am assigning argv a variable and 3 arguments
    but i am getting an error message everytime.
    also trying to use input with arguments.
    have tried a couple of differnet times but always get an error.

---
+++ attempt13.1.py
@@ -0,0 +1,7 @@
+from sys import argv
+one, two, three, four = argv
+print("Intresting.", one)
+print("Indeed", two)
+no = input(nope)
+print("Do you get it?", three , {no})
+print("heres the number", four , 'silly')
---
+++ attempt13.py
@@ -0,0 +1,9 @@
+from sys import argv
+dead,kix,bowl,spoon = argv
+settigns = input()
+settings2 = input()
+print("Do you enjoy the:", dead)
+print("Do you like:", kix)
+print("Do you use a:", spoon)
+print(f"Do you:", dead , '{settings}')
+print("Or do you dig the:", dead, 'in {settings2} more')
