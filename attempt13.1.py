b542802ccc2f64cf3b5d39676d97c85257752363
Parsa <alh.parsa@gmail.com>
2018-03-22 01:12:01 +0800

    general description for 13.1
    I've made some changes to 13.1, try to fix 13 using the comments on 13.1

--- attempt13.1.py
+++ attempt13.1.py
@@ -1,7 +1,32 @@
 from sys import argv
-one, two, three, four = argv
-print("Intresting.", one)
-print("Indeed", two)
-no = input(nope)
-print("Do you get it?", three , {no})
-print("heres the number", four , 'silly')
+'''
+
+Note: This program can't be executed through input console!
+
+Normally if you don't use an IDE you run python like this:
+    'cd Project_directory'
+    On mac and linux:
+    'python Filename.py'
+    On windows:
+    'python_version.exe filename.py'
+
+As you may probably now, argv gets arguments from user within a script. To provide 
+arguments you are required to run the code through command line. When the code is executed 
+that way your first argument is your filename. More arguments can be added as such:
+    'python_version.exe filename.py first_arg second_arg third_arg'
+
+
+'''
+
+
+'''
+    # filename: String, gets file's name from the command line
+    # first_arg: String, gets the first argument from user
+
+'''
+filename, first_arg, sec_arg, third_arg = argv
+
+print("Your file name", filename)
+print("Your first argument", first_arg)
+print("Your second argument?", sec_arg)
+print("Your third argument", third_arg)
