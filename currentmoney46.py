b86fe881c9680289d6ff88fafb9876d41df06a9f
deadhead65 <37427871+deadhead65@users.noreply.github.com>
2018-04-07 12:02:48 -0400

    Add files via upload

---
+++ currentmoney46.py
@@ -0,0 +1,12 @@
+def bills(grocries,gas,autozone,walmart2,current):
+    return
+grocries = 75
+gas = 36.17
+autozone = 6.88
+walmart2 = 1.78
+current = 1030.00
+bills = grocries - gas - autozone - walmart2 - current
+print("This is how much money i have left"f'\n {bills}')
+
+
+
